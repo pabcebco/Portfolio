6806935a287ddf1f67be47c426c8763e3f55c95e
pabcebco <72361197+pabcebco@users.noreply.github.com>
2020-10-05 00:48:08 +0200

    Add files via upload

---
+++ SQL Queries/share_sakila.sql
@@ -0,0 +1,22 @@
+-- Permite calcular la cuota de venta por categoría y mes --
+-- Para esta querie he empleado el esquema "sakila", preinstalado en MySQL --
+
+SELECT 
+	month(p.payment_date) as MES, 
+    c.name as CATEGORIA,
+    SUM(p.amount) as TOTAL,
+    SUM(p.amount)/AVG(TotMes)*100 as SHARE
+FROM payment as p
+	LEFT JOIN rental USING (rental_id)
+	LEFT JOIN inventory USING (inventory_id)
+	LEFT JOIN film_category USING (film_id)
+	LEFT JOIN category as c USING (category_id)
+    LEFT JOIN 
+		(SELECT
+		month (p2.payment_date) as messq1, 
+        SUM(p2.amount) as TotMes
+		FROM payment as p2
+		GROUP BY month (p2.payment_date) ) as sq1 
+        ON month (p.payment_date) = messq1
+GROUP BY MES, CATEGORIA
+ORDER BY MES, SHARE DESC;
