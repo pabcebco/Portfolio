807d719151ec7d42ff3046acf6c5c48609855273
pabcebco <72361197+pabcebco@users.noreply.github.com>
2020-10-12 22:26:00 +0200

    Update Conversion de gastos.py

--- Python/Conversion de gastos.py
+++ Python/Conversion de gastos.py
@@ -1,109 +1,56 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Simple ETL process for a basic home expenses table
 
-# In[111]:
-
-
+# Import pandas and read the csv where the expenses are (downloaded from your bank)
 import pandas as pd
-gastos_sin_editar = pd.read_csv('datasets/gastos/gastos.csv', sep=";", decimal = ",", dayfirst = True)
+gastos_sin_editar = pd.read_csv('datasets/gastos.csv', sep=";", decimal = ",", dayfirst = True)
+
+# Keeping the relevant columns
 gastos2=gastos_sin_editar[["Fecha_Valor","Concepto","Importe"]]
 
+# Changing the name of the columns using a dictionary
+gastos2 = gastos2.rename(columns={"Fecha_Valor":"DATE", "Concepto":"CONCEPT", "Importe":"IMPORT"})
 
-# In[113]:
-
-
-gastos2 = gastos2.rename(columns={"Fecha_Valor":"DATE", 
-                        "Concepto":"CONCEPT", 
-                        "Importe":"IMPORT"})
+# Adding a new column in case observations are needed
 gastos2.loc[:,"OBSERVATION"] = 'NaN'
 
-
-# In[114]:
-
-
+# Creating and performing boolean masks filtering
 mask = gastos2['IMPORT'] > 0
 gastos2.loc[mask, 'CLASS'] = 'Income'
 gastos2.loc[~mask, 'CLASS'] = 'Expense'
-
-
-# In[115]:
-
 
 mask = gastos2['CONCEPT'].str.lower().str.find('pepe mobil') >= 0
 gastos2.loc[mask, 'TYPE'] = 'Phone'
 gastos2.loc[mask, 'SUBTYPE'] = 'Pablo'
 
-
-# In[ ]:
-
-
 mask = gastos2['CONCEPT'].str.lower().str.find('yoigo') >= 0
 gastos2.loc[mask, 'TYPE'] = 'Phone'
 gastos2.loc[mask, 'SUBTYPE'] = 'Nilu'
-
-
-# In[116]:
-
 
 mask = gastos2['CONCEPT'].str.lower().str.find('consum') >= 0
 gastos2.loc[mask, 'TYPE'] = 'Food'
 gastos2.loc[mask, 'SUBTYPE'] = 'Groceries'
 
-
-# In[117]:
-
-
-mask = gastos2['CONCEPT'].str.lower().str.find('just') >= 0
+mask = gastos2['CONCEPT'].str.lower().str.find('just-eat') >= 0
 gastos2.loc[mask, 'TYPE'] = 'Food'
 gastos2.loc[mask, 'SUBTYPE'] = 'Delivery'
 
-
-# In[ ]:
-
-
-mask = gastos2['CONCEPT'].str.lower().str.find('dominos') >= 0
+mask = gastos2['CONCEPT'].str.lower().str.find('dominos pizza') >= 0
 gastos2.loc[mask, 'TYPE'] = 'Food'
 gastos2.loc[mask, 'SUBTYPE'] = 'Delivery'
-
-
-# In[ ]:
-
 
 mask = gastos2['CONCEPT'].str.lower().str.find('farmacia') >= 0
 gastos2.loc[mask, 'TYPE'] = 'Pharmacy'
-
-
-# In[ ]:
-
 
 mask = gastos2['CONCEPT'].str.lower().str.find('ecooltra') >= 0
 gastos2.loc[mask, 'TYPE'] = 'Movility'
 gastos2.loc[mask, 'SUBTYPE'] = 'Motorbike'
 
-
-# In[ ]:
-
-
 mask = gastos2['CONCEPT'].str.lower().str.find('taxi') >= 0
 gastos2.loc[mask, 'TYPE'] = 'Movility'
 gastos2.loc[mask, 'SUBTYPE'] = 'Motorbike'
 
-
-# In[ ]:
-
-
 mask = gastos2['IMPORT'] = 675
 gastos2.loc[mask, 'TYPE'] = 'Rental'
 
-
-# In[110]:
-
-
-gastos2.to_csv('datasets/Gastos/gastos_editado.csv', sep=";", decimal = ",", date_format = '%Y/%m/%d')
-
-
-# In[ ]:
-
-
-
-
+# Exporting the results to a new dataset to be visualized
+gastos2.to_csv('datasets/gastos_editado.csv', sep=";", decimal = ",", date_format = '%Y/%m/%d')
